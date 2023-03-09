33357e8a20db84a82b2a821b8747832ff10ab62d
ddogga <hayeonlee60327@gmail.com>
2023-03-09 20:56:49 +0900

    initialize

---
+++ src/test/java/com/admin/shop3/Shop3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin.shop3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shop3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
